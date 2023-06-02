829a03b819dab6ce716b4f303443179cd867a3a5
leesohun <leesohun99@gmail.com>
2023-06-02 11:19:17 +0900

    덧셈 추가

--- HelloTest.java
+++ HelloTest.java
@@ -1,7 +1,8 @@
 public class HelloTest {
    public static void main(String[] args) {
       System.out.println("Hello Git!!");
-      int sum =a -b;
+      int sum = a -b;
+      int sum = a + b;
 
    }
 }
